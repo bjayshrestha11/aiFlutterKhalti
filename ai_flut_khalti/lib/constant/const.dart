653a236dc88a927f7121d4d607d069b19701b60a
aistha11 <bijayshrestha0908@gmail.com>
2021-11-24 19:04:40 +0545

    Done

--- ai_flut_khalti/lib/constant/const.dart
+++ ai_flut_khalti/lib/constant/const.dart
@@ -1,3 +1,3 @@
 
-
-const String KHALTI_API = "test_public_key_bc1030a98de440e694f61f2e593dceb7";
+// get your own khalti api key from merchant account
+const String KHALTI_API = "test_public_key_bc1030a98deasdfadff61f2e593dceb7";
